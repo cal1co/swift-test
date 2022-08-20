3d1a983e1592039784fbdb47163380b6c2fd23cf
Alex King <calicoalix@gmail.com>
2022-08-20 19:11:59 +1000

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Alex King on R 4/08/20.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
